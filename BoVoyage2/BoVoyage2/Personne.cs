2b207b265e941d1e7421fd44a1b6926ef6eb5867
TL <lst.thierry@gmail.com>
2018-08-30 13:00:09 +0200

    Creation d'un Menu d'acces au system depuis la classe Program

--- BoVoyage2/BoVoyage2/Personne.cs
+++ BoVoyage2/BoVoyage2/Personne.cs
@@ -1,8 +1,11 @@
 ﻿using System;
+using System.IO; // espace de nom pour l'utilisation de liste sous forme de fichier si besoin
 
-public class Class1
+public class Personne // renommage de Class1 en Personne
 {
-	public Class1()
-	{
+    public abstract Personne() // classe abstraite qui nous envoie sur les classe Client et Participant
+    {
+        Personne.Client = new Client;
+        Personne.Participant = news Participant; // à corriger
 	}
 }
--- BoVoyage2/BoVoyage2/Program.cs
+++ BoVoyage2/BoVoyage2/Program.cs
@@ -3,15 +3,45 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO; // espace de nom pour l'utilisation de liste sous forme de fichier si besoin
+
 
 namespace BoVoyage2
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main(string[] args) // creation du Menu d'entrée dans le systeme
         {
-            class abstract Personne();
-               Personne {get;set;} // faire un appel sur les classes client et participant pour obtenir les listes ensuite
+          Classes.Menus.PageAccueil();
+
+            bool continuer = true;
+            while (continuer)
+                 {
+                var mGesCial = Classes.Menus.MenuGestionCommerciale();
+                switch (mGesCial)
+                {
+                    case "1":
+                        Classes.Menus.MenuGestionVoyages();
+                        break;
+
+                    case "2":
+                        Classes.Menus.MenuGestionClients();
+                        break;
+
+                    case "q":
+                    case "Q":
+                        continuer = false;
+                        break;
+
+                    default:
+                        Classes.Esthetisme.MiseEnFormeTexte("Choix invalide, l'application va fermer", ConsoleColor.Red, centre: false);
+
+                        continuer = false;
+                        break;
+                }
+            }
+            Console.ReadKey();
+            Console.Clear();
         }
     }
 }
