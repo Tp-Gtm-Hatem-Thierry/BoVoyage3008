f09c294338234ef513cee4e75e935f5dd692e8b8
HatemSat <hatem.saturi@gmail.com>
2018-08-30 15:40:30 +0200

    Creation Menu

--- BoVoyageQuiMarche/BoVoyageQuiMarche/Classes/Participant.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace BoVoyageQuiMarche
-{
-    public class Participant
-    {
-
-    }
-    
-}
--- BoVoyageQuiMarche/BoVoyageQuiMarche/Program.cs
+++ BoVoyageQuiMarche/BoVoyageQuiMarche/Program.cs
@@ -10,10 +10,108 @@ namespace BoVoyageQuiMarche
     {
         static void Main(string[] args)
         {
+            bool continuer = true;
+            while (continuer)
+            {
+                var choix1 = MenuPrincipal();
+                switch (choix1)
+                {
+                    case "1":
+                        bool continuer2 = true;
+                        Console.Clear();
+                        while (continuer2)
+                        {
+
+                            var choix2 = MenuVoyage();
+                            switch (choix2)
+                            {
+                                case "1":
+
+                                    //ListeOffresA();
+                                    break;
+                                case "2":
+                                    break;
+                                case "3":
+                                    break;
+                                case "4":
+                                    continuer2 = false;
+                                    break;
+                            }
+                        }
+                        break;
+                    case "2":
+                        bool continuer3 = true;
+                        Console.Clear();
+                        while (continuer3)
+                        {
+
+                            var choix3 = MenuClient();
+                            switch (choix3)
+                            {
+                                case "1":
+                                    //ListeOffres();
+                                    break;
+                                case "2":
+                                    break;
+                                case "3":
+                                    break;
+                                case "4":
+                                    continuer3 = false;
+                                    break;
+                            }
+                        }
+                        break;
+                    case "3":
+                        continuer = false;
+                        break;
+                    default:
+                        Console.WriteLine("Choix invalide, veuiller recommencer");
+                        Console.ReadKey();
+                        //continuer = true;
+                        break;
+                }
+            }
+        }
+        static string MenuPrincipal()
+        {
+            Console.Clear();
+
+            Console.WriteLine("BoVoyage\n");
+            Console.WriteLine("MENU PRINCIPAL\n");
+            Console.WriteLine("1. Gestionnaire de voyage\n");
+            Console.WriteLine("2. Gestionnaire clientèle\n");
+            Console.WriteLine("3. Quitter BoVoyage");
+            Console.Write("\nVotre choix : ");
+
+
+            return Console.ReadLine();
+        }
+        static string MenuVoyage()
+        {
+
+            Console.WriteLine("Gestionnaire Voyage\n\n");
+            Console.WriteLine("1. Liste des offres\n");
+            Console.WriteLine("2. Ajouter une offre\n");
+            Console.WriteLine("3. Supprimer une offre\n");
+            Console.WriteLine("4. Retour");
+            Console.Write("\nVotre choix : ");
+
+            return Console.ReadLine();
 
         }
+        static string MenuClient()
+        {
 
-   
+            Console.WriteLine("Gestionnaire Clientèle\n\n");
+            Console.WriteLine("1. Nouvelles reservations\n");
+            Console.WriteLine("2. Liste Clients\n");
+            Console.WriteLine("3. Campagne emailing\n");
+            Console.WriteLine("4. Retour");
+
+            return Console.ReadLine();
+        }
+
+
     }
     
 }
