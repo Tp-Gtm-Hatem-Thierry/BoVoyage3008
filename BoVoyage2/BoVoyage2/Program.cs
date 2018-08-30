901015e878031801ffc23198fb3312f33a4ea603
TL <lst.thierry@gmail.com>
2018-08-30 13:09:32 +0200

    Ajout du mdp

--- BoVoyage2/BoVoyage2/Program.cs
+++ BoVoyage2/BoVoyage2/Program.cs
@@ -10,9 +10,25 @@ namespace BoVoyage2
 {
     class Program
     {
-        static void Main(string[] args) // creation du Menu d'entrée dans le systeme
+        static void Main(string[] args) // creation du Menu d'entrée dans le systeme avec acces par mdp
         {
           Classes.Menus.PageAccueil();
+
+            Console.WriteLine("\nVotre identifiant qui est votre prenom, je vous pries (Alex)");
+            var name = Console.ReadLine();
+            var date = DateTime.Now;
+
+            if (name != "Alex") //une liste de commercial par exemple
+            {
+                Console.WriteLine("\n\aVous n'avez pas acces, désolé !");
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine($"\nBienvenu {name}, veuillez entrez inscrir votre mot de passe");
+                var mdp = Console.ReadLine();
+                if (mdp == "UserCom")//liste de mdp egalement...
+                Console.WriteLine($"\n\nQue souhaitez vous réaliser aujourd'hui {name}?\n");
 
             bool continuer = true;
             while (continuer)
