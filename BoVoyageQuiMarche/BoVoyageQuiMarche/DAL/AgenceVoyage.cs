35ac5657fed8537dd83c209b9ec8bd32d7d01f6a
TL <lst.thierry@gmail.com>
2018-08-31 09:54:34 +0200

    repush suite à problem de synchro githup

---
+++ BoVoyageQuiMarche/BoVoyageQuiMarche/DAL/AgenceVoyage.cs
@@ -0,0 +1,44 @@
+﻿namespace BoVoyageQuiMarche
+{
+    public class AgenceDeVoyage
+    {
+        public void Id(Id id)
+        {
+            //var date = DateTime.Now; // Affichage de la date
+            using (var contexte = new Contexte())
+            {
+                contexte.Id.Add(id);
+                contexte.SaveChanges();
+            }
+        }
+
+        public void Nom(Nom nom) // ajouter le namespace DAL
+        {
+            using (var contexte = new Contexte())
+            {
+                contexte.Entry(nom).State = EntityState.Deleted;
+                contexte.SaveChanges();
+            }
+        }
+
+        public IEnumerable<Id> ListerId()
+        {
+            using (var contexte = new Contexte())
+            {
+                return contexte.Id // recup les bonnes info de la table
+                    .Include(x => x.adapter)//adapter
+                    .OrderBy(x => x.Nom).ToList();
+            }
+        }
+
+        public Id GetId(int idId)
+        {
+            using (var contexte = new Contexte())
+            {
+                return contexte.Id
+                    .Include(x => x.adapter)// adapter
+                    .Single(x => x.Id == idId);
+            }
+        }
+    }
+}
